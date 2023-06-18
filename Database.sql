f982b53f3be7bbfe8672452232a2e7f3d61a6bf0
Tabbo98 <robbi.tab@gmail.com>
2023-06-19 01:37:45 +0200

    FIX DB 14

--- Database.sql
+++ Database.sql
@@ -90,7 +90,7 @@ CREATE TABLE Inserimento (
 CREATE TABLE Invito (
     id INT AUTO_INCREMENT,
     email VARCHAR(255),
-    codice INT(10),
+    codice INT(10) NOT NULL,
     dominio VARCHAR(50),
     esito VARCHAR(255),
     PRIMARY KEY (id, email, codice, dominio),
