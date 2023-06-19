748eeb2fa842f183237d7349ad85555a33fede92
mengo1234 <104265848+mengo1234@users.noreply.github.com>
2023-06-19 17:53:12 +0200

    Add files via upload

--- PagineWeb/Profilo.php
+++ PagineWeb/Profilo.php
@@ -4,101 +4,65 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="C:\Users\Administrator\Desktop\Basi-Di-dati\CSSPagineWeb\Profilo.css" />
+  <link rel="stylesheet" href="C:\xampp\htdocs\Basi-Di-dati-Ramo170623\Basi-Di-dati-Ramo170623\CSSPagineWeb\Profilo.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
   <title>Profilo</title>
-    <?php
-
-    $mysqli=new mysqli("localhost","root","", "sondaggi23");
-    $query= "SELECT * FROM utente";
-    $res= $mysqli->query($query) or die (mysqli_error($con));
-    $result= $mysqli->query($query) or die (mysqli_error($con));
-    while ($rowS= mysqli_fetch_array($result)){
-
-        $mail=$rowS["email"];
-        $nome=$rowS["nome"];
-        $cognome=$rowS["cognome"];
-        $anno=$rowS["anno"];
-        $luogoNascita=$rowS["luogoNascita"];
-        $totaleBonus=$rowS["totaleBonus"];
-    }
-    ?>
-
-
 </head>
 
+<body>
+    <!-- verifica se l'utente ha fatto il log in se no reindirizzamento a pagina di login-->
+    <?php
 
-
+   session_start();
+    if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
+        // L'utente non è autenticato, mostra l'alert e reindirizza alla pagina desiderata
+        echo '<script>alert("Devi effettuare l\'accesso per visualizzare questa pagina."); window.location.href = "AccediRegistrati.php";</script>';
+        exit();
+    }?>
 
    <!-- freccia di ritorno -->
-   <a href="C:\Users\Administrator\Desktop\Basi-Di-dati\Home.html" class="back-button">
+   <a href="C:\Users\Administrator\Desktop\Basi-Di-dati\Home.php" class="back-button">
    <i class="fas fa-arrow-left"></i> </a>
 
   <!-- Intestazione della pagina -->
   <h1>Profilo</h1>
 
-</p>
   <!-- Bottone "Dati Personali" -->
   <button onclick="showDatiPersonali()">Dati Personali</button>
 
   <!-- Bottone "Invita a Sondaggio" -->
   <button onclick="showInvitaSondaggio()">Invita a Sondaggio</button>
+  
+  <!-- Bottone "Premi" -->
+<button onclick="showPremi()">Premi Ottenuti</button>
 
   <!-- Tabella Dati Personali (inizialmente nascosta) -->
-<div id="datiPersonaliTable">
+  <div id="datiPersonaliTable" style="display: none;">
     <h2>Dati Personali</h2>
     <table>
-        <thead>
+      <thead>
         <tr>
-            <th>Nome</th>
-            <th>Cognome</th>
-            <th>Email</th>
+          <th>Nome</th>
+          <th>Cognome</th>
+          <th>Email</th>
         </tr>
-        </thead>
-        <tbody>
-        <?php
-        // Connessione al database
-        $conn = mysqli_connect("localhost", "root", "", "sondaggi23");
-
-        // Recupera l'email dell'utente corrente (puoi passare questa variabile come parametro alla pagina)
-        $email = $_GET['email'];
-
-        // Esegue una query per recuperare i dati dell'utente
-        $query = "SELECT * FROM users WHERE email = '$email'";
-        $result = mysqli_query($conn, $query);
-
-        // Mostra i dati dell'utente
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo "<tr>";
-            echo "<td>" . $row['nome'] . "</td>";
-            echo "<td>" . $row['cognome'] . "</td>";
-            echo "<td>" . $row['email'] . "</td>";
-            echo "</tr>";
-        }
-
-        // Chiude la connessione al database
-        mysqli_close($conn);
-        ?>
-        </tbody>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Mario</td>
+          <td>Rossi</td>
+          <td>mario@example.com</td>
+        </tr>
+        <tr>
+          <td>Paola</td>
+          <td>Verdi</td>
+          <td>paola@example.com</td>
+        </tr>
+      </tbody>
     </table>
-</div>
-
-<script>
-    // Nasconde tutte le righe della tabella tranne quella con l'email dell'utente corrente
-    var email = "<?php echo $email ?>";
-    var rows = document.querySelectorAll("#datiPersonaliTable tbody tr");
-    rows.forEach(function(row) {
-        row.style.display = "none";
-        if (row.querySelector("td:last-child").textContent == email) {
-            row.style.display = "table-row";
-        }
-    });
-</script>
+  </div>
 
   <!-- Tabella Invita a Sondaggio (inizialmente nascosta) -->
-
-
-
   <div id="invitaSondaggioTable" style="display: none;">
     <h2>Invita a Sondaggio</h2>
     <table>
@@ -110,32 +74,66 @@
       </thead>
       <tbody>
         <tr>
-          <td> <?php
-              $inc=0;
-              while ($row= mysqli_fetch_array($res)) {
-                  echo "mail:".($inc+1)."<br><a href='#' onclick=\"document.getElementById('email".$inc."').submit(); return false;\">".$row["email"]."</a><form id='email".$inc."' method='post' action='salva_click.php'><input type='hidden' name='email' value='".$row["email"]."'></form><br>";
-                  $inc++;
-              }
-              ?>
-          </td>
+          <td>Giovanni</td>
+          <td>giovanni@example.com</td>
+        </tr>
+        <tr>
+          <td>Sara</td>
+          <td>sara@example.com</td>
         </tr>
       </tbody>
     </table>
   </div>
+
+  <!-- Tabella Premi Ottenuti (inizialmente nascosta) -->
+<div id="premiOttenutiTable" style="display: none;">
+  <h2>Premi Ottenuti</h2>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Descrizione</th>
+        <th>Foto</th>
+        <th>Punti Necessari</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>1</td>
+        <td>Premio 1</td>
+        <td><img src="foto1.jpg" alt="Foto Premio 1"></td>
+        <td>100</td>
+      </tr>
+      <tr>
+        <td>2</td>
+        <td>Premio 2</td>
+        <td><img src="foto2.jpg" alt="Foto Premio 2"></td>
+        <td>200</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
 
   <!-- Script JavaScript per mostrare/nascondere le tabelle -->
   <script>
     function showDatiPersonali() {
       document.getElementById("datiPersonaliTable").style.display = "block";
       document.getElementById("invitaSondaggioTable").style.display = "none";
+      document.getElementById("premiOttenutiTable").style.display = "none";
     }
 
     function showInvitaSondaggio() {
       document.getElementById("datiPersonaliTable").style.display = "none";
       document.getElementById("invitaSondaggioTable").style.display = "block";
+      document.getElementById("premiOttenutiTable").style.display = "none";
     }
-  </script>
 
+    function showPremi() {
+    document.getElementById("datiPersonaliTable").style.display = "none";
+    document.getElementById("invitaSondaggioTable").style.display = "none";
+    document.getElementById("premiOttenutiTable").style.display = "block";
+  }
+  </script>
 </body>
 
 </html>
